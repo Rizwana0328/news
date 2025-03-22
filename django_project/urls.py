8017980037f0adbf5c4d02d58b9fd40474bb2613
Rizwana0328 <rizwana.umar1@gmail.com>
2025-03-22 10:09:29 -0400

    add Bootstrao styling

--- django_project/urls.py
+++ django_project/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"),
     name="home"),
-
+    path("", include("pages.urls")),
 ]
 
