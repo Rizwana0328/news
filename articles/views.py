7db4b4cc5294f1ef7d3441e49cd977f2069ef015
Rizwana0328 <rizwana.umar1@gmail.com>
2025-04-04 12:36:15 -0400

    permissions and authorizations

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView  # new
+from django.contrib.auth.mixins import LoginRequiredMixin  # new
 from django.views.generic.edit import UpdateView, DeleteView, CreateView  # new
 from django.urls import reverse_lazy  # new
 from .models import Article
@@ -22,18 +23,28 @@ class ArticleUpdateView(UpdateView):  # new
     )
     template_name = "article_edit.html"
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
 
 class ArticleDeleteView(DeleteView):  # new
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleCreateView(CreateView):  # new
+class ArticleCreateView(LoginRequiredMixin, CreateView):  # new
     model = Article
     template_name = "article_new.html"
     fields = (
         "title",
-        "body",
-        "author",
+        "body"
     )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
